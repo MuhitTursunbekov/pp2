9acd82a645a753db154ca5f08a81be66050031d8
MuhitTursunbekov <m_tursunbekov@kbtu.kz>
2024-01-22 19:15:40 +0600

    example1

--- lab 1/data types/example1.py
+++ lab 1/data types/example1.py
@@ -1,2 +1,49 @@
 x = 5
 print(type(x))
+
+x = "Hello World"
+#str
+
+x = 20
+#int
+
+x = 20.5
+#float
+
+x = 1j
+#complex
+
+x = ["apple", "banana", "cherry"]
+#list
+
+x = ("apple", "banana", "cherry")
+#tuple 
+
+x = range(6)
+#range
+
+x = {"name" : "John", "age" : 36}
+#dict
+
+x = {"apple", "banana", "cherry"}
+#set
+
+x = frozenset({"apple", "banana", "cherry"})
+#frozenset
+
+x = True
+#bool
+
+x = b"Hello"
+#bytes
+
+x = bytearray(5)
+#bytearray
+
+x = memoryview(bytes(5))
+#memoryview
+
+x = None
+#NoneType
+
+
