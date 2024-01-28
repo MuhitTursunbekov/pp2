f726aeaaabde2571ba39fa631470a67482c3b0ae
MuhitTursunbekov <m_tursunbekov@kbtu.kz>
2024-01-28 22:30:39 +0600

    for loops

---
+++ lab2/while loops.py
@@ -0,0 +1,63 @@
+#examples
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+  i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+#exercises
+  
+
+i = 1
+while i < 6:
+ print(i)
+i += 1
+
+
+i = 1
+while i < 6:
+  if i == 3:
+    break
+i += 1
+
+
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
