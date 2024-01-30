28fb25bf4433061d4e15920549979a34f7ae59eb
MuhitTursunbekov <m_tursunbekov@kbtu.kz>
2024-01-30 11:12:32 +0600

    functions

---
+++ lab3/functions.py
@@ -0,0 +1,131 @@
+#examples
+
+
+def my_function():
+  print("Hello from a function")
+
+
+def my_function():
+  print("Hello from a function")
+my_function()
+
+
+def my_function(fname):
+  print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+my_function("Emil", "Refsnes")
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+my_function("Emil", "Tobias", "Linus")
+
+
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+my_function(fname = "Tobias", lname = "Refsnes")
+
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+
+def my_function(food):
+  for x in food:
+    print(x)
+fruits = ["apple", "banana", "cherry"]
+my_function(fruits)
+
+
+def my_function(x):
+  return 5 * x
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
+def myfunction():
+  pass
+
+
+def my_function(x, /):
+  print(x)
+my_function(3)
+
+
+def my_function(x):
+  print(x)
+my_function(x = 3)
+
+
+def my_function(*, x):
+  print(x)
+my_function(x = 3)
+
+
+def my_function(x):
+  print(x)
+my_function(3)
+
+
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+my_function(5, 6, c = 7, d = 8)
+
+
+def tri_recursion(k):
+  if(k > 0):
+    result = k + tri_recursion(k - 1)
+    print(result)
+  else:
+    result = 0
+  return result
+print("\n\nRecursion Example Results")
+tri_recursion(6)
+
+
+#exercises
+
+
+
+def my_function():
+  print("Hello from a function")
+
+
+def my_function():
+  print("Hello from a function")
+my_function()
+
+
+
+def my_function(fname, lname):
+  print(fname)
+
+
+def my_function(x):
+ return x+5
+
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+
